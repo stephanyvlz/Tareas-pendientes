ee18ef98c42cb6a0de70c988b20c56c44cf247b3
Stephany Vlz <5323100091@correcaminos.uts.edu.mx>
2024-08-15 19:57:44 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Tareas-pendientes
-Proyecto realizado durante el bootcamp de Tecnolochicas Pro, pagina responsiva con Html, Css, Bootstrap y Javascript
+# Lista de Tareas
+## Proyecto responsivo realizado con Bootstrap 5, JavaScript y C#
+
+Proyecto creado durando el *bootcamp* de _Tecnolochicas Pro_ y es adaptable a distintos dispositivos o tamaños de pantalla.
+El propósito de esta página web es __mostrar un listado de pendientes para realizar__.💛
+
+Aquí podrás acceder al proyecto [Tareas-Pendientes](https://github.com/stephanyvlz/Tareas-pendientes)
+
+### Tecnologías utilizadas:
+
+* HTML
+* CSS
+* BOOTSTRAP 5
+* JAVASCRIPT
+
+### Cuenta con...
+Botones responsivos, cambio de tema, agregar, tachar y eliminar objeto de pendiente y un apartado para mostrar la fecha actual.
---
+++ main.js
@@ -0,0 +1,111 @@
+// definir las constantes
+
+const fecha = document.querySelector('#fecha');
+const lista = document.querySelector('#lista');
+const elemento = document.querySelector('#elemento');
+const input = document.querySelector('#input');
+const botonAgregar = document.querySelector('#botonAgregar');
+const check = 'bi-record-circle';
+const tachado = 'tachado';
+const uncheck = 'bi-circle';
+let LIST;
+let id;
+
+const FECHA = new Date ();
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric'
+});
+// DOM 
+
+// Función agregar tarea
+function agregarTarea(tarea,id,hecho,eliminar) {
+    if (eliminar) {
+        return
+    };
+
+    const realizado = hecho ? check : uncheck;
+    const LINE = hecho ? tachado : '' ;
+    const elemento = `<li id="elemento">
+                    <i id="${id}" data="hecho" class="bi ${realizado}"></i>
+                    <p class="tarea-lista text ${LINE}">${tarea}</p>
+                    <i id="${id}" data="eliminar" class="bi bi-x"></i>
+                </li> `
+                lista.insertAdjacentHTML("beforeend",elemento);
+
+};
+
+    function tareaRealizada(element) {
+
+        element.classList.toggle(check);
+        element.classList.toggle(uncheck);
+        
+        element.parentNode.querySelector('.text').classList.toggle(tachado);
+        LIST[element.id].realizado = LIST[element.id].realizado ?false :true;
+    }
+function tareaEliminada(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].eliminar = true;
+    
+};
+window.addEventListener('load', () => {
+const tema = document.getElementById('tema');
+tema.addEventListener("click", () => {
+    const estilo1 = document.getElementById('style1');
+    const estilo2 = document.getElementById('style2');
+
+    if (estilo1.disabled) {
+        estilo1.disabled = false;
+        estilo2.disabled = true;
+    } else {
+        estilo1.disabled = true;
+        estilo2.disabled = false;
+    }
+})
+});
+
+botonAgregar.addEventListener("click", () => {
+    const tarea = input.value;
+    if (tarea) {
+        agregarTarea(tarea, id, false, false)
+        LIST.push({
+            nombre: tarea,
+            id: id,
+            hecho: false,
+            eliminar: false,
+        });
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+        id++;
+        input.value = "";
+
+    }
+});
+
+lista.addEventListener("click", function (event){
+    const element = event.target;
+    const elementData = element.attributes.data.value;
+    if (elementData == "hecho") {
+        tareaRealizada(element);
+    } else if (elementData == "eliminar") { 
+        tareaEliminada(element);
+    };
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
+
+let data = localStorage.getItem ("TODO");
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    cargarLista(LIST);
+} else {
+    LIST = [];
+    id = 0; 
+}
+function cargarLista(array) {
+    array.forEach(
+        function (item){
+            agregarTarea(item.nombre, item.id, item.hecho, item.eliminar);
+        }
+    )
+};
